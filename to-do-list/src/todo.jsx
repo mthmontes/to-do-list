34385aeb6663da244c0307620d06a87da2ca407e
“mthmontes“ <plutaozaum@gmail.com>
2022-09-13 14:24:22 -0300

    file modified

--- to-do-list/src/todo.jsx
+++ to-do-list/src/todo.jsx
@@ -14,6 +14,18 @@ const Todo = () => {
         setTodos(todosArray);
     }
 
+    // const completeTodo = (index) => {
+    //     const todosArray = [...todos];
+    //     todosArray[index].completed = !todosArray[index].completed;
+    //     setTodos(todosArray);
+    // }
+
+    // const deleteTodo = (index) => {
+    //     let todosArray = [...todos];
+    //     todosArray.splice(index, 1);
+    //     setTodos(todosArray);
+    // }
+
     return(
       <div>
         <input className='input-todo' value={currentTodo} onChange={e => {
@@ -30,6 +42,8 @@ const Todo = () => {
         {todos.map((todo, index) => 
             <div key={todo} className='todo'>
                 <div className='checkbox' onClick={() => completeTodo(index)}>{todo.completed && <span>&#x2714;</span>}</div>
+                {/* <div className={todo.completed ? 'done' : ""}>{todo.todo}</div>
+                <div className="delete" onClick={() => deleteTodo(index)}>&#128465;</div> */}
                 
             </div>
         )}
